515175acb72d8400bbf67d25f2f1de3c18213832
Vinoth Selvaraju <vinothse@buffalo.edu>
2014-02-11 19:25:27 -0500

    query parser updated for create table command

--- src/edu/buffalo/cse562/controller/queryParser.java
+++ src/edu/buffalo/cse562/controller/queryParser.java
@@ -1,13 +1,17 @@
 package edu.buffalo.cse562.controller;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserManager;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 import net.sf.jsqlparser.statement.select.Select;
 import edu.buffalo.cse562.logger.logManager;
@@ -41,11 +45,28 @@ public class queryParser {
 		Iterator<String> queryIte = sqlQueryList.iterator();
 		while(queryIte.hasNext()){
 			CCJSqlParserManager parser = new CCJSqlParserManager();
+			Map<String,ArrayList<String>> tableColumnMap = new HashMap<String,ArrayList<String>>();
 			try {
 				Statement statement = parser.parse(new StringReader(queryIte.next()));
 				lg.logger.log(Level.INFO, statement.toString());
+				
+				/*@author - vino
+				 * logic - create a hashMap with table name as the key and array list of column names as value
+				 */
 				if(statement instanceof CreateTable) {
-					
+					System.out.println("QUERY TYPE: create");
+					CreateTable createTableStatement = (CreateTable) statement;
+					ArrayList<String> columnNameList = new ArrayList<String>();
+					ArrayList<ColumnDefinition> columnDefinitionList = (ArrayList) createTableStatement.getColumnDefinitions();
+					for (ColumnDefinition s : columnDefinitionList){
+						columnNameList.add(s.getColumnName());
+					}
+					//Adding table name and column names to the map
+					tableColumnMap.put(createTableStatement.getTable().getName(), columnNameList);
+					//Printing the contents of the HashMap
+					for (Map.Entry<String, ArrayList<String>> entry : tableColumnMap.entrySet()){
+						System.out.println(entry.getKey() + "/" + entry.getValue());
+					}	
 				} else if(statement instanceof Select) {
 					
 				}
