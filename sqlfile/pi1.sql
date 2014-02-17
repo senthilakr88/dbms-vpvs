33e8472b2551992b9fd50568b602fdbb26f52e08
alagappanr <ralagappan2006@gmail.com>
2014-02-17 01:52:53 -0500

    Testing branching

--- sqlfile/pi1.sql
+++ sqlfile/pi1.sql
@@ -1,2 +1,3 @@
 CREATE TABLE personInfo(id int, first_name string, last_name string, Gender string, age int, State string)
 SELECT id, first_name, last_name, age from personInfo where (age+10)>=30
+SELECT id, first_name, last_name, age from personInfo where (age+10)>=30
--- src/edu/buffalo/cse562/physicalPlan/SelectionOperator.java
+++ src/edu/buffalo/cse562/physicalPlan/SelectionOperator.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
 
+import edu.buffalo.cse562.logger.logManager;
+import edu.buffalo.cse562.sql.expression.evaluator.CalcTools;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.ExpressionVisitor;
 
 public class SelectionOperator implements Operator {
 	
@@ -21,27 +25,39 @@ public class SelectionOperator implements Operator {
 	@Override
 	public void resetStream() {
 		input.resetStream();
-
 	}
 
 	@Override
 	public Tuple readOneTuple() {
+		//System.out.println("Came here");
 		Tuple t = null;
 		do {
+			//System.out.println("Doing now");
 			t = input.readOneTuple();
 			if(t == null) {
 				return null;
 			}
 			if(!evaluate(t,condition)) {
+				System.out.println("Condition not Satisfied");
 				t = null;
+			} else {
+				System.out.println("Condition satisfied");
 			}
 		} while(t==null);
+		//System.out.println("Came here 2");
 		return t;
 	}
 	
 	private boolean evaluate(Tuple t, Expression condition2) {
+		logManager lg = new logManager();
 		// TODO Auto-generated method stub
-		return false;
+		lg.logger.log(Level.INFO, "Tototo"+ t.toString());
+		lg.logger.log(Level.INFO, "Tototo"+ t.tableMap.toString());
+		//t.tableMap.get(key)
+		CalcTools calc = new CalcTools(t);
+		lg.logger.log(Level.INFO, "OYOYOYOYOYOYOYOYO");
+		condition2.accept(calc);
+		return calc.getAccumulatorBoolean();
 	}
 
 }
