30d122eeacc980080f156112c5468807db7f9d30
Palaniappan Meiyappan <palaniap@buffalo.edu>
2014-02-21 20:46:19 -0500

    Components modified to incorporate check

--- src/edu/buffalo/cse562/logicalPlan/components.java
+++ src/edu/buffalo/cse562/logicalPlan/components.java
@@ -11,6 +11,8 @@ import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.select.FromItem;
 import net.sf.jsqlparser.statement.select.Join;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectBody;
 import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 import edu.buffalo.cse562.logger.logManager;
@@ -36,9 +38,9 @@ public class components {
 	SelectBody selectBody;
 
 	public components() {
-		
+
 		tableMap = new ArrayList<Column>();
-		
+
 		tableColTypeMap = new HashMap<String, ArrayList<String>>();
 		lg = new logManager();
 	}
@@ -55,8 +57,8 @@ public class components {
 	public void addWhereConditions(Expression where) {
 		whereClause = where;
 	}
-	
-	public void setSelectBody(SelectBody selectBody){
+
+	public void setSelectBody(SelectBody selectBody) {
 		this.selectBody = selectBody;
 	}
 
@@ -64,67 +66,71 @@ public class components {
 		StringBuffer toPrint = new StringBuffer();
 		toPrint.append("PROJECT [(" + projectStmt + ")]\n");
 		toPrint.append("SELECT [" + whereClause + "\n");
-//		for (Map.Entry<String, ArrayList<String>> entry : tableMap.entrySet()) {
-//			toPrint.append("SCAN [" + entry.getKey() + "(" + entry.getValue()
-//					+ ")]");
-//		}
+		// for (Map.Entry<String, ArrayList<String>> entry :
+		// tableMap.entrySet()) {
+		// toPrint.append("SCAN [" + entry.getKey() + "(" + entry.getValue()
+		// + ")]");
+		// }
 		return toPrint.toString();
 	}
 
 	public void executePhysicalPlan() {
 		Table table = (Table) tableName;
-		Operator oper = new FileScanOperator(table, tableDir, tableMap, tableColTypeMap);
-		
-		if(tableJoins != null) {
+		Operator oper = new FileScanOperator(table, tableDir, tableMap,
+				tableColTypeMap);
+
+		if (tableJoins != null) {
 			Iterator joinIte = tableJoins.iterator();
-			while(joinIte.hasNext()) {
+			while (joinIte.hasNext()) {
 				Join joinTable = (Join) joinIte.next();
-				Operator rightOper = new FileScanOperator((Table)joinTable.getRightItem(), tableDir, tableMap, tableColTypeMap);
-				oper = new JoinOperator(oper, rightOper);
+				Operator rightOper = new FileScanOperator(
+						(Table) joinTable.getRightItem(), tableDir, tableMap,
+						tableColTypeMap);
+				oper = new JoinOperator(oper, rightOper,
+						joinTable.getOnExpression());
+
 			}
 		}
-		
-		if (!whereClause.equals(null)){
+
+		if (!whereClause.equals(null)) {
 			oper = new SelectionOperator(oper, whereClause);
-			
+
+		}
+
+		if (((PlainSelect) selectBody).getGroupByColumnReferences() != null) {
+			// Aggregate computation.
+			oper.resetStream();
+			oper = new AggregateOperator(oper, selectBody, tableMap);
+			Datum[] test = oper.readOneTuple();
+			test = oper.readOneTuple();
+			test = oper.readOneTuple();
+			test = oper.readOneTuple();
+			System.out.println("PRINTING TUPLE FROM AGGREGATE OPERATOR");
+			printTuple(test);
 		}
 		
-		
-		//Aggregate computation.
-		oper.resetStream();
-		oper = new AggregateOperator(oper,selectBody,tableMap);
-		Datum[] test = oper.readOneTuple();
-		test = oper.readOneTuple();
-		test = oper.readOneTuple();
-		test = oper.readOneTuple();
-		System.out.println("PRINTING TUPLE FROM AGGREGATE OPERATOR");
-		printTuple(test);
-		
-		//Projection computation
-		/*oper=new ProjectionOperator(oper,projectStmt);
+		// Projection computation
+		oper = new ProjectionOperator(oper, projectStmt);
 		Datum[] t = oper.readOneTuple();
-//		tableMap = ((ProjectionOperator)oper).getTableMap();
-				
 		while (t != null) {
-////			System.out.println(t.toString());
 			printTuple(t);
 			t = oper.readOneTuple();
-		}*/
+		}
 
 	}
 
 	private void printTuple(Datum[] row) {
 		Boolean first = true;
-		if(row!=null && row.length !=0) {
-		for(Datum col : row) {
-			if(!first)
-			System.out.print("|"+col );
-			else {
-				System.out.print(col);
-				first = false;
+		if (row != null && row.length != 0) {
+			for (Datum col : row) {
+				if (!first)
+					System.out.print("|" + col);
+				else {
+					System.out.print(col);
+					first = false;
+				}
 			}
-		}
-		System.out.println();
+			System.out.println();
 		}
 	}
 
@@ -151,12 +157,12 @@ public class components {
 
 	public void addColsToTable(ArrayList<Column> columnNameList) {
 		tableMap.addAll(columnNameList);
-		
+
 	}
 
 	public void addJoins(List joins) {
 		this.tableJoins = (ArrayList) joins;
-		
+
 	}
 
 }
