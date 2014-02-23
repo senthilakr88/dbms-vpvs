ad7a7c0cd726cf1a98fe5b18a41a0b9c36063568
Palaniappan Meiyappan <palaniap@buffalo.edu>
2014-02-23 15:43:22 -0500

    original sql files

--- sqlfile/tpch3.sql
+++ sqlfile/tpch3.sql
@@ -38,15 +38,18 @@ CREATE TABLE CUSTOMER (
         comment      VARCHAR(117)
     );
 SELECT
-  customer.name,
-  lineitem.orderkey
+  lineitem.orderkey,
+  sum(lineitem.extendedprice*(1-lineitem.discount)) as revenue, 
+  orders.orderdate,
+  orders.shippriority
 FROM
   customer,
   orders,
   lineitem 
 WHERE
-  customer.mktsegment = 'BUILDING'  and customer.custkey = orders.custkey
-  and lineitem.orderkey = orders.orderkey
+  customer.mktsegment = 'BUILDING' and customer.custkey = orders.custkey
+  and lineitem.orderkey = orders.orderkey 
   and orders.orderdate < DATE('1995-03-15')
   and lineitem.shipdate > DATE('1995-03-15')
-  order by lineitem.orderkey;
+GROUP BY lineitem.orderkey, orders.orderdate, orders.shippriority 
+ORDER BY revenue desc, orders.orderdate;
--- sqlfile/tpch6.sql
+++ sqlfile/tpch6.sql
@@ -17,21 +17,11 @@ CREATE TABLE LINEITEM (
         comment        VARCHAR(44)
     );
 select
-shipdate,
-discount,
-extendedprice,
-extendedprice*discount,
-sum(extendedprice*discount),
-count(shipdate),
-avg(extendedprice*discount) as revenue,
-avg(extendedprice) as revenue
+sum(extendedprice*discount) as revenue
 from
 lineitem
 where
 shipdate >= DATE('1994-01-01')
 and shipdate < date ('1995-01-01')
-	and discount >0.06 - 0.01 
-and discount<0.06+ 0.01 
---and quantity < 24
-;
+and discount >0.06 - 0.01 and discount<0.06+ 0.01 and quantity < 24;
 
