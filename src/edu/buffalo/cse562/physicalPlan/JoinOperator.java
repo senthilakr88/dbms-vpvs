7abe5b29496fa06b289528d52e433bdd214c4557
Palaniappan Meiyappan <palaniap@buffalo.edu>
2014-02-25 12:04:04 -0500

    Nested Join improvised

--- src/edu/buffalo/cse562/physicalPlan/JoinOperator.java
+++ src/edu/buffalo/cse562/physicalPlan/JoinOperator.java
@@ -15,21 +15,19 @@ public class JoinOperator implements Operator {
 	Boolean firstEntry;
 	Expression expr;
 	boolean isTupleMapPresent;
-	List<Datum[]> buffer;
-	Integer bufferMaxSize;
-	Integer bufferPointer;
-	Boolean isEnd;
+	List<Datum[]> leftBuffer;
+	List<Datum[]> rightBuffer;
+	Integer BufferMaxSize;
+	Integer leftBufCursor, rightBufCursor;
 
 	public JoinOperator(Operator left, Operator right, Expression expression) {
 		this.left = left;
 		this.right = right;
-		this.firstEntry = true;
 		this.expr = expression;
 		this.isTupleMapPresent = true;
-		this.bufferMaxSize = 10000;
-		this.bufferPointer = -1;
-		this.buffer = new ArrayList<Datum[]>(bufferMaxSize);
-		this.isEnd = false;
+		this.BufferMaxSize = 3;
+		this.leftBuffer = fillLeftBuffer();
+		this.rightBuffer = fillRightBuffer();
 	}
 
 	@Override
@@ -41,76 +39,155 @@ public class JoinOperator implements Operator {
 
 		Datum[] lt = null, rt = null;
 		Datum[] t = null;
-//		System.out.println("Inside :: " +bufferPointer + " :: " + buffer.size() + " :: " + bufferPointer.compareTo(buffer.size()));
-		if (bufferPointer.compareTo(buffer.size()-1) != 0 && buffer.size() <= bufferMaxSize) {
-//			System.out.println(bufferPointer);
-			++bufferPointer;
-//			printTuple(buffer.get(bufferPointer));
-			return buffer.get(bufferPointer);
-		} else {
-			
-			bufferPointer = -1;
-			buffer = new ArrayList<Datum[]>(bufferMaxSize);
-			while (buffer.size() < bufferMaxSize && !isEnd) {
-				// long startTime = System.currentTimeMillis();
-//				System.out.println("Filling buffer :: " + buffer.size());
-				if (firstEntry) {
-					lt = left.readOneTuple();
+//		System.out.println("inside join");
+		long startTime = System.nanoTime();
+		do {
+			//System.out.println("entering loop" + firstEntry);
+			if (firstEntry) {
+				lt = getLeftDatum();
+				setTuple(lt);
+				firstEntry = false;
+			}
+			lt = getTuple();
+			if (lt != null) {
+				//System.out.println("left is not empty");
+				rt = getRightDatum();
+				if (rt == null) {
+					//System.out.println("right is empty");
+					lt = getLeftDatum();
+					if(lt==null) {
+						//System.out.println("both block end");
+						////System.out.println("entering left null");
+						resetLeftBuf();
+						//System.out.println("reset left");
+						lt = getLeftDatum();
+						
+						rightBuffer = fillRightBuffer();
+						//System.out.println("fill right");
+					} else {
+						resetRightBuf();
+						//System.out.println("reset right");
+					}
 					setTuple(lt);
-					// System.out.println("entry");
-					firstEntry = false;
-				}
-				lt = getTuple();
-				if (lt != null) {
-					rt = right.readOneTuple();
-					if (rt == null) {
-						lt = left.readOneTuple();
-
-						if (lt == null) {
-							isEnd = true;
-							break;
+					rt = getRightDatum();
+//					//System.out.println(lt.toString());
+//					//System.out.println(rt.toString());
+					if(rt == null) {
+						//System.out.println("right is again empty");
+						//System.out.println("fill left");
+						leftBuffer = fillLeftBuffer();
+						if(isBufEmpty(leftBuffer)){
+							System.out.println("both stream end");
+							setTuple(null);
+							return null;
 						}
-						setTuple(lt);
 						right.resetStream();
-						rt = right.readOneTuple();
+						rightBuffer = fillRightBuffer();
+//						//System.out.println("Fill right");
+						continue;
 					}
-					// On expression evaluation
-					t = combine(lt, rt);
-					//printTuple(t);
-					if (t == null) {
-						return null;
-					}
-					if (!evaluate(t, expr)) {
-						t = null;
-					}
-					
-					if (t != null) {
-						//printTuple(t);
-						buffer.add(t);
-					}
+				} else {
+					//System.out.println("right is not empty");
+				}
+				////System.out.println("combining");
+				t = combine(lt, rt);
 
-				} else {
+				if (t == null) {
+					
 					return null;
 				}
+				if (!evaluate(t, expr)) {
+					t = null;
+				}
+			}  else {
+				return null;
 			}
+//			System.out.println("in join");
+		} while (t == null);
+//		System.out.println("Outside join :: " + String.valueOf(System.nanoTime() - startTime));
+		return t;
+	}
+
+	public Datum[] getLeftDatum() {
+		Datum[] lt;
+		
+		if(leftBuffer == null)
+			return null;
+		++leftBufCursor;
+		if (leftBufCursor < leftBuffer.size()) {
+			lt = leftBuffer.get(leftBufCursor);
+		} else {
+			lt = null;
 		}
-//		System.out.println("Outside :: " +bufferPointer + " :: " + buffer.size() + " :: " + bufferPointer.compareTo(buffer.size()));
-			if(!isEnd){
-			++bufferPointer;
-//			printTuple(buffer.get(bufferPointer));
-			return buffer.get(bufferPointer);
-			} else {
-				if (bufferPointer.compareTo(buffer.size()-1) != 0 && buffer.size() <= bufferMaxSize) {
-//					System.out.println(bufferPointer);
-					++bufferPointer;
-//					printTuple(buffer.get(bufferPointer));
-					return buffer.get(bufferPointer);
-				} else {
-					return null;
-				}
-			}
+		return lt;
+	}
+
+	public Datum[] getRightDatum() {
+		Datum[] rt;
+		
+		if(rightBuffer == null) {
+			//System.out.println("Datum return :: rightbuffer empty");
+			return null;
+		}
+		++rightBufCursor;
+		if (rightBufCursor < rightBuffer.size()) {
+			//System.out.println("getting rightbuffer value");
+			rt = rightBuffer.get(rightBufCursor);
+		} else {
+			rt = null;
+		}
+		return rt;
 	}
 	
+	public void resetRightBuf() {
+		rightBufCursor = -1;
+	}
+	
+	public void resetLeftBuf() {
+		leftBufCursor = -1;
+	}
+
+	public ArrayList<Datum[]> fillLeftBuffer() {
+		//System.out.println("entering left buffer for filling");
+		ArrayList<Datum[]> tempBuf = new ArrayList<Datum[]>();
+		Datum[] tempDatum;
+		this.firstEntry = true;
+		this.leftBufCursor = -1;
+		tempDatum = left.readOneTuple();
+		if(tempDatum == null)
+			return null;
+		while (tempDatum != null) {
+			tempBuf.add(tempDatum);
+			//System.out.println(tempDatum);
+			if(tempBuf.size() == BufferMaxSize)
+				return tempBuf;
+			tempDatum = left.readOneTuple();
+		}
+		return tempBuf;
+	}
+
+	public ArrayList<Datum[]> fillRightBuffer() {
+		//System.out.println("entering right buffer for filling");
+		ArrayList<Datum[]> tempBuf = new ArrayList<Datum[]>();
+		Datum[] tempDatum;
+		this.rightBufCursor = -1;
+		tempDatum = right.readOneTuple();
+		if(tempDatum == null)
+			return null;
+		while (tempDatum != null) {
+			tempBuf.add(tempDatum);
+			//System.out.println(tempDatum);
+			if(tempBuf.size() == BufferMaxSize)
+				return tempBuf;
+			tempDatum = right.readOneTuple();
+		}
+		return tempBuf;
+	}
+	
+	public boolean isBufEmpty(List<Datum[]> buffer) {
+		return (buffer == null || buffer.size() == 0) ? true : false;
+	}
+
 	private void printTuple(Datum[] row) {
 		Boolean first = true;
 		if (row != null && row.length != 0) {
@@ -122,13 +199,8 @@ public class JoinOperator implements Operator {
 					first = false;
 				}
 			}
-			System.out.println();
+			//System.out.println();
 		}
-	}
-
-	private Datum[] tupleBufFull() {
-		++bufferPointer;
-		return buffer.get(bufferPointer);
 	}
 
 	private boolean evaluate(Datum[] t, Expression expr2) {
@@ -140,7 +212,7 @@ public class JoinOperator implements Operator {
 			}
 			CalcTools calc = new CalcTools(t);
 			expr.accept(calc);
-			// System.out.println(calc.getAccumulatorBoolean());
+			// //System.out.println(calc.getAccumulatorBoolean());
 			return calc.getAccumulatorBoolean();
 		} else {
 			return true;
@@ -149,16 +221,20 @@ public class JoinOperator implements Operator {
 
 	private Datum[] combine(Datum[] lt, Datum[] rt) {
 		int i = 0, j = 0;
-		Datum[] temp = new Datum[lt.length + rt.length];
-
+		int len = 0;
+		len += lt.length;
+		len += rt.length;
+		Datum[] temp = new Datum[len];
+		
 		for (i = 0; i < lt.length; i++) {
 			temp[i] = lt[i];
-			// System.out.println(lt[i].toComString());
+			// //System.out.println(lt[i].toComString());
 		}
 		for (j = 0; j < rt.length; j++, i++) {
 			temp[i] = rt[j];
-			// System.out.println(rt[j].toComString());
+			// //System.out.println(rt[j].toComString());
 		}
+		//System.out.println("Combining :: " + lt[0] + " :: " +rt[0]);
 		return temp;
 	}
 
