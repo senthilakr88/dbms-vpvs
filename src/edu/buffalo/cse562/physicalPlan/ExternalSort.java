047a311d676f3ed245f4ecebf22348088cfff65a
Palaniappan Meiyappan <palaniap@buffalo.edu>
2014-04-08 00:35:14 -0400

    Removing sysout

--- src/edu/buffalo/cse562/physicalPlan/ExternalSort.java
+++ src/edu/buffalo/cse562/physicalPlan/ExternalSort.java
@@ -371,7 +371,7 @@ public class ExternalSort implements Operator {
 								// }
 								
 								check = false;
-								System.out.println("Changing check value :: " + check);
+//								System.out.println("Changing check value :: " + check);
 								list1 = null;
 								indexTraversal.clear();
 								break;
